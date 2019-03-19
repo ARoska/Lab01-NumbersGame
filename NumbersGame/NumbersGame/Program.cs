a5832c4997f95f9c25c6c0004aa1376fa8f09986
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-19 09:33:10 -0700

    Main() populated.

--- NumbersGame/NumbersGame/Program.cs
+++ NumbersGame/NumbersGame/Program.cs
@@ -6,7 +6,41 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong :(");
+                Console.WriteLine($"Error message: {e}");
+            }
+
+        }
+
+        static void StartSequence()
+        {
+
+        }
+
+        static void Populate()
+        {
+
+        }
+
+        static void GetSum()
+        {
+
+        }
+
+        static void GetProduct()
+        {
+
+        }
+
+        static void GetQuotient()
+        {
+
         }
     }
 }
